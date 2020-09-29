d04f21f2b24592c44df712a136bbc630cb4540b8
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-09-28 23:04:27 -0300

    Estruturando a validação

---
+++ validacao_formulario/README.md
@@ -0,0 +1,4 @@
+# Descrição: Diversos projetos em HTML, CSS e Javascript puro
+
+## Projeto 1: Sistema de login com validação
+
--- validacao_formulario/script.js
+++ validacao_formulario/script.js
@@ -1,4 +1,53 @@
 let input = document.querySelectorAll("form .focus");
+let form = document.querySelector(".validator");
+let validator = {
+    handleSubmit:(event) => {
+        event.preventDefault();
+        let send = true;
+        let inputs = form.querySelectorAll("input");
+
+        for(i = 0; i< inputs.length; i++) {
+            let input = inputs[i];
+            let check = validator.checkInput(input);
+            
+            if(check !== true) {
+                send = false;
+                console.log(check);
+                
+            }
+        }
+
+        if(send) {
+            form.submit();
+        }
+
+    }, 
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+
+        if(rules !== 'null') {
+            rules = rules.split("|");
+            for(rule in rules) {
+                let ruleDetails = rules[rule].split("=");
+                switch(ruleDetails[0]) {
+                    case 'required':
+                        if(input.value == '') {
+                            return 'Campo não pode ser vazio';
+                        }
+                    break;
+                    case 'min': 
+
+                    break;
+                        
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+form.addEventListener("submit", validator.handleSubmit);
 
 input.forEach(item => {
     item.addEventListener("focus", () => {
@@ -9,5 +58,5 @@ input.forEach(item => {
 input.forEach(item => {
     item.addEventListener("blur", () => {
         item.style.borderColor = "#CCC";
-    })
-})
+    });
+});
