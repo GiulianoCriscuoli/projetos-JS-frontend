64dad956c7211f60810120e9757cf555af00416a
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-09-30 10:59:34 -0300

    Exibindo erros

--- validacao_formulario/script.js
+++ validacao_formulario/script.js
@@ -6,13 +6,15 @@ let validator = {
         let send = true;
         let inputs = form.querySelectorAll("input");
 
+        validator.clearErrors();
+
         for(i = 0; i< inputs.length; i++) {
             let input = inputs[i];
             let check = validator.checkInput(input);
             
             if(check !== true) {
                 send = false;
-                console.log(check);
+                validator.showErrors(input, check);
                 
             }
         }
@@ -25,14 +27,14 @@ let validator = {
     checkInput:(input) => {
         let rules = input.getAttribute('data-rules');
 
-        if(rules !== 'null') {
+        if(rules !== null) {
             rules = rules.split("|");
-            for(rule in rules) {
+            for(let rule in rules) {
                 let ruleDetails = rules[rule].split("=");
                 switch(ruleDetails[0]) {
                     case 'required':
-                        if(input.value == '') {
-                            return 'Campo não pode ser vazio';
+                        if(input.value === '') {
+                            return 'Campo não pode ser vazio!';
                         }
                     break;
                     case 'min': 
@@ -44,6 +46,26 @@ let validator = {
         }
 
         return true;
+    },
+    showErrors:(input, error) => {
+        input.style.borderColor = "#FF0000";
+
+        let errorElement = document.createElement("div");
+        errorElement.classList.add("error");
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i = 0; i < inputs.length; i++) {
+            inputs[i].stye = '';
+        }
+        let errorElement = document.querySelectorAll(".error");
+        for(let i = 0; i < errorElement.length; i++) {
+            errorElement[i].remove();
+        }
     }
 }
 
